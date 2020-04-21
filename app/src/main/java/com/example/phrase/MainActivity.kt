cda8f41e0c19247a3eea7c5e655581d6ffd35638
belvi nosakhare <nosakharebelvi@gmail.com>
2020-04-21 10:50:47 -0400

    testing options

--- app/src/main/java/com/example/phrase/MainActivity.kt
+++ app/src/main/java/com/example/phrase/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.belvi.phrase.Phrase
+import xyz.belvi.phrase.options.PhraseOptions
 import xyz.belvi.phrase.translateMedium.medium.GoogleTranslate
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val phrase = Phrase.with(GoogleTranslate(this, R.raw.credential, "fr"))
+            .options(PhraseOptions.options().build())
             .setUp()
 
         button.setOnClickListener {
