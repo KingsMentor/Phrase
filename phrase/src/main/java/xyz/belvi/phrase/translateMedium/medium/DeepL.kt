f0d8fc1d7e392375613727a1ad6955a8111cf7bd
belvi nosakhare <nosakharebelvi@gmail.com>
2020-06-20 00:41:02 -0400

    support for deepL

--- phrase/src/main/java/xyz/belvi/phrase/translateMedium/medium/DeepL.kt
+++ phrase/src/main/java/xyz/belvi/phrase/translateMedium/medium/DeepL.kt
@@ -1,19 +1,52 @@
 package xyz.belvi.phrase.translateMedium.medium
 
 import xyz.belvi.phrase.options.PhraseDetected
+import xyz.belvi.phrase.translateMedium.Languages
 import xyz.belvi.phrase.translateMedium.TranslationMedium
+import xyz.belvi.phrase.translateMedium.medium.retrofit.ApiClient
+import xyz.belvi.phrase.translateMedium.medium.retrofit.DeepLApi
 
-final class DeepL(val apiKey: String) : TranslationMedium() {
+class DeepL(private val apiKey: String) : TranslationMedium() {
 
-    override suspend fun translate(text: String, sourceLanguage: String, targeting: String): String {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    private val mURL = "https://api.deepl.com/"
+
+    override suspend fun translate(
+        text: String,
+        sourceLanguage: String,
+        targeting: String
+    ): String {
+        val key = "$sourceLanguage:$targeting:$text"
+        if (cacheTranslation.containsKey(key))
+            return cacheTranslation[key]!!
+        val deepLTranslation = ApiClient.retrofit(mURL).create(DeepLApi::class.java)
+            .translate(apiKey, text, targeting).translations.firstOrNull()
+        val result = deepLTranslation?.text ?: ""
+        cacheTranslation[key] = result
+        return result
     }
 
     override fun name(): String {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+        return "DeepL"
     }
 
-    override suspend fun detect(text: String): PhraseDetected? {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    override suspend fun detect(text: String, targeting: String): PhraseDetected? {
+        if (cacheDetected.containsKey(text))
+            return cacheDetected[text]!!
+        val deepLTranslation = ApiClient.retrofit(mURL).create(DeepLApi::class.java)
+            .translate(apiKey, text, targeting).translations.firstOrNull()
+        val detect = deepLTranslation?.detected_source_language ?: ""
+        val result = PhraseDetected(
+            text,
+            detect,
+            Languages.values().find { it.code == detect }?.name ?: detect,
+            name()
+        )
+        cacheDetected[text] = result
+        val key = "$detect:$targeting:$text"
+        cacheTranslation[key] = deepLTranslation?.text ?: ""
+        return result
     }
+
+    data class DeepLTranslation(val detected_source_language: String, val text: String)
+    data class DeepLTranslationResponse(val translations: List<DeepLTranslation>)
 }
